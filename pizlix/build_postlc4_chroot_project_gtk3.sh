0b8c52199feeae9cf2634c5844004c8cfc111277
Fil Pizlix <pizlo@mac.com>
2026-07-06 09:46:52 -0700

    Enable introspection for gtk3 build (Gdk-3.0/Gtk-3.0 typelibs now generated)

--- pizlix/build_postlc4_chroot_project_gtk3.sh
+++ pizlix/build_postlc4_chroot_project_gtk3.sh
@@ -8,7 +8,7 @@ tar -xf pizlonated-gtk3.tar.gz
 cd pizlonated-gtk3
 mkdir -v build
 cd build
-meson setup .. --prefix=/usr --buildtype=debugoptimized -D broadway_backend=true -D x11_backend=false -D wayland_backend=true -D introspection=false --wrap-mode=nofallback
+meson setup .. --prefix=/usr --buildtype=debugoptimized -D broadway_backend=true -D x11_backend=false -D wayland_backend=true --wrap-mode=nofallback
 ninja
 ninja install
 cd ../..

03a6c8d2aaad9ac2b9f281186f52fc75e60129a7
Fil Pizlo <pizlo@mac.com>
2026-07-24 12:49:17 -0700

    Turn off forgiving dyncast now that I fixed the -Bsymbolic-functions problem in LibreOffice.

--- filc/tests/dyncastshlib/main.cpp
+++ filc/tests/dyncastshlib/main.cpp
@@ -7,7 +7,7 @@ __attribute__((visibility("default"))) int main() {
     void* obj = create_child();
     fprintf(stderr, "=== Same lib (should succeed) ===\n");
     test_cast_same_lib(obj);
-    fprintf(stderr, "=== Different lib (should succeed but fails if libc++abi is configured incorrectly) ===\n");
+    fprintf(stderr, "=== Different lib (succeeds or fails depending on libc++abi configuration) ===\n");
     test_cast_diff_lib(obj);
     return 0;
 }

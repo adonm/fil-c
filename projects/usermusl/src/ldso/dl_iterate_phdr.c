32b163d7bd34f0174734841f5c9dda607b53fb43
Fil Pizlo <pizlo@mac.com>
2026-08-21 23:07:35 -0700

    Revert "dl: dl_iterate_phdr support with Fil-C-safe dl_phdr_info"
    This reverts commit dbbea0c565c99f02879d848ed516ccf3db90e607.

--- projects/usermusl/src/ldso/dl_iterate_phdr.c
+++ projects/usermusl/src/ldso/dl_iterate_phdr.c
@@ -2,15 +2,11 @@
 #include <link.h>
 #include "pthread_impl.h"
 #include "libc.h"
-#include <pizlonated_syscalls.h>
+#include <stdfil.h>
 
-/* The Fil-C runtime enumerates the loaded objects (via the host dl_iterate_phdr) and builds, for
-   each one, a `struct dl_phdr_info` whose `dlpi_name` and `dlpi_phdr` pointers carry valid Fil-C
-   capabilities before invoking `callback`. Doing this in the runtime is required: the phdrs live in
-   the host's ELF image, which user (Fil-C) code cannot legally form a pointer to. */
 static int static_dl_iterate_phdr(int(*callback)(struct dl_phdr_info *info, size_t size, void *data), void *data)
 {
-	return zsys_dl_iterate_phdr((void *)callback, data);
+    zerror("static_dl_iterate_phdr not implemented");
 }
 
 weak_alias(static_dl_iterate_phdr, dl_iterate_phdr);

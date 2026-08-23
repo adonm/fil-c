12eed943bdbc7e57255c18b4eb9dddfa157a1c2a
Fil Pizlo <pizlo@mac.com>
2026-08-23 03:44:45 +0000

    Switch pizlix to git 2.55.0

--- pizlix/build_postlc2_sub1_packaging.sh
+++ pizlix/build_postlc2_sub1_packaging.sh
@@ -21,7 +21,7 @@ rm -vf projects/*/pizlonated-*.tar.gz
 ./package-source.sh projects/curl-8.21.0 pizlonated-curl
 ./package-source.sh projects/emacs-30.1 pizlonated-emacs
 ./package-source.sh projects/sudo-1.9.17p2 pizlonated-sudo
-./package-source.sh projects/git-2.46.0 pizlonated-git
+./package-source.sh projects/git-2.55.0 pizlonated-git
 ./package-source.sh projects/libuv-1.51.0 pizlonated-libuv
 ./package-source.sh projects/icu-76.1 pizlonated-icu
 ./package-source.sh projects/libxml2-2.14.4 pizlonated-libxml2

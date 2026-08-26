e4134341c2916cabab147d31250f2660570267b1
Fil Pizlo <pizlo@mac.com>
2026-08-25 19:15:15 -0700

    Remove stale versions of packages

--- projects/Linux-PAM-1.7.1/aux/chdir_meson_build_subdir.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -efu
-
-exe=$1; shift
-exe=$(readlink -ev -- "$exe")
-cd "$MESON_BUILD_SUBDIR"
-exec "$exe" "$@"
--- projects/Linux-PAM-1.7.1/aux/redir_exe.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -efu
-# stdin stdout ...
-
-exec < "$1"; shift
-exec > "$1"; shift
-exec "$@"

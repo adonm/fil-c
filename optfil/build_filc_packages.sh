513c28580437d5ece3748ec665746aa7f96301c2
Fil Pizlo <pizlo@mac.com>
2026-09-03 06:55:23 +0000

    Switch optfil to curl 8.22.0

--- optfil/build_filc_packages.sh
+++ optfil/build_filc_packages.sh
@@ -94,7 +94,7 @@ cd ../..
 ./package-source.sh projects/libidn2-2.3.7 pizlonated-libidn2
 ./package-source.sh projects/libtasn1-4.21.0 pizlonated-libtasn1
 ./package-source.sh projects/p11-kit-0.26.5 pizlonated-p11-kit
-./package-source.sh projects/curl-8.21.0 pizlonated-curl
+./package-source.sh projects/curl-8.22.0 pizlonated-curl
 ./package-source.sh projects/git-2.55.0 pizlonated-git
 ./package-source.sh projects/libevent-2.1.13 pizlonated-libevent
 ./package-source.sh projects/m4-1.4.19 pizlonated-m4
--- optfil/build_opt.sh
+++ optfil/build_opt.sh
@@ -870,7 +870,7 @@ cd ..
 rm -rf nghttp2-1.70.0
 hash -r
 
-tar -xf $FILCSRC/projects/curl-8.21.0/pizlonated-curl.tar.gz
+tar -xf $FILCSRC/projects/curl-8.22.0/pizlonated-curl.tar.gz
 cd pizlonated-curl
 CC=/opt/fil/bin/filcc CXX=/opt/fil/bin/fil++ ./configure --prefix=/opt/fil --sysconfdir=/etc --disable-static --with-openssl --enable-threaded-resolver --with-ca-path=/etc/ssl/certs --with-gsssapi
 make -j `nproc`

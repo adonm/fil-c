33bbdefde2f391ea82f944b05be4abbf10ebd154
Fil Pizlo <pizlo@mac.com>
2026-09-03 02:41:41 +0000

    Fil-C changes to curl-8.22.0 since 01346829096c61b372692f6dc43ffa778c6caccd

--- projects/curl-8.22.0/lib/vtls/vtls_config.c
+++ projects/curl-8.22.0/lib/vtls/vtls_config.c
@@ -43,6 +43,7 @@
 #include "urldata.h"
 #include "setopt.h"
 #include "strcase.h"
+#include "curl_ca_bundle.h"
 #include "vtls/vtls.h"
 #include "vtls/vtls_config.h"
 
@@ -249,9 +250,7 @@ CURLcode Curl_ssl_easy_config_complete(struct Curl_easy *data,
                                        struct Curl_peer *origin)
 {
   struct ssl_config_data *sslc = &data->set.ssl;
-#if defined(CURL_CA_PATH) || defined(CURL_CA_BUNDLE)
   CURLcode result;
-#endif
 
   ssl_easy_config_compl_options(origin, data->state.initial_origin, sslc);
 
@@ -260,20 +259,22 @@ CURLcode Curl_ssl_easy_config_complete(struct Curl_easy *data,
     if(!sslc->custom_capath && !sslc->custom_cafile && !sslc->custom_cablob)
       sslc->primary.native_ca_store = TRUE;
 #endif
-#ifdef CURL_CA_PATH
     if(!sslc->custom_capath && !CURL_EASY_STR(data, STRING_SSL_CAPATH)) {
-      result = Curl_setstropt(data, STRING_SSL_CAPATH, CURL_CA_PATH);
-      if(result)
-        return result;
+      const char *capath = Curl_ca_path();
+      if(capath) {
+        result = Curl_setstropt(data, STRING_SSL_CAPATH, capath);
+        if(result)
+          return result;
+      }
     }
-#endif
-#ifdef CURL_CA_BUNDLE
     if(!sslc->custom_cafile && !CURL_EASY_STR(data, STRING_SSL_CAFILE)) {
-      result = Curl_setstropt(data, STRING_SSL_CAFILE, CURL_CA_BUNDLE);
-      if(result)
-        return result;
+      const char *bundle = Curl_ca_bundle();
+      if(bundle) {
+        result = Curl_setstropt(data, STRING_SSL_CAFILE, bundle);
+        if(result)
+          return result;
+      }
     }
-#endif
   }
   sslc->primary.CAfile = ssl_easy_steal(data, STRING_SSL_CAFILE);
   sslc->primary.CRLfile = ssl_easy_steal(data, STRING_SSL_CRLFILE);
@@ -321,22 +322,24 @@ CURLcode Curl_ssl_easy_config_complete(struct Curl_easy *data,
     if(!sslc->custom_capath && !sslc->custom_cafile && !sslc->custom_cablob)
       sslc->primary.native_ca_store = TRUE;
 #endif
-#ifdef CURL_CA_PATH
     if(!sslc->custom_capath &&
        !CURL_EASY_STR(data, STRING_SSL_CAPATH_PROXY)) {
-      result = Curl_setstropt(data, STRING_SSL_CAPATH_PROXY, CURL_CA_PATH);
-      if(result)
-        return result;
+      const char *capath = Curl_ca_path();
+      if(capath) {
+        result = Curl_setstropt(data, STRING_SSL_CAPATH_PROXY, capath);
+        if(result)
+          return result;
+      }
     }
-#endif
-#ifdef CURL_CA_BUNDLE
     if(!sslc->custom_cafile &&
        !CURL_EASY_STR(data, STRING_SSL_CAFILE_PROXY)) {
-      result = Curl_setstropt(data, STRING_SSL_CAFILE_PROXY, CURL_CA_BUNDLE);
-      if(result)
-        return result;
+      const char *bundle = Curl_ca_bundle();
+      if(bundle) {
+        result = Curl_setstropt(data, STRING_SSL_CAFILE_PROXY, bundle);
+        if(result)
+          return result;
+      }
     }
-#endif
   }
   sslc->primary.CAfile = ssl_easy_steal(data, STRING_SSL_CAFILE_PROXY);
   sslc->primary.CApath = ssl_easy_steal(data, STRING_SSL_CAPATH_PROXY);

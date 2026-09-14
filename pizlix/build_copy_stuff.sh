de9c360e27998007a05eb986581645f00265fbbb
Fil Pizlo <pizlo@mac.com>
2026-09-14 17:13:20 +0000

    Rebase coreutils to 9.12 in projeny mode.

--- pizlix/build_copy_stuff.sh
+++ pizlix/build_copy_stuff.sh
@@ -38,8 +38,7 @@ cp -v \
     bzip2-1.0.8-install_docs-1.patch \
     bzip2-1.0.8.tar.gz \
     check-0.15.2.tar.gz \
-    coreutils-9.11-i18n-1.patch \
-    coreutils-9.11.tar.xz \
+    coreutils-9.12.tar.xz \
     curl-8.22.0.tar.xz \
     dejagnu-1.6.3.tar.gz \
     dhcpcd-10.0.8.tar.xz \
